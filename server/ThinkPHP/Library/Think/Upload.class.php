99c16162d8cd40ea6407dd1104f8f688d3fc2603
RedBoneZhang <zqzzhong@qq.com>
2021-09-15 09:48:49 +0800

    保存文件时使用上传文件名称

--- server/ThinkPHP/Library/Think/Upload.class.php
+++ server/ThinkPHP/Library/Think/Upload.class.php
@@ -22,7 +22,8 @@ class Upload {
         'subName'       =>  array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
         'rootPath'      =>  './Uploads/', //保存根路径
         'savePath'      =>  '', //保存路径
-        'saveName'      =>  array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
+        /*'saveName'      =>  array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组*/
+        'saveName'      =>  array(), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
         'saveExt'       =>  '', //文件保存后缀，空则使用原后缀
         'replace'       =>  false, //存在同名是否覆盖
         'hash'          =>  true, //是否生成hash编码
